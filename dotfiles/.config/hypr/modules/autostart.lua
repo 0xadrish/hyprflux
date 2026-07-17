99499d2c724361baf223c24dc7004a1738043e71
Adrish <adrish.codes@gmail.com>
2026-07-17 18:06:24 +0530

    style: configure Bibata cursor theme

--- dotfiles/.config/hypr/modules/autostart.lua
+++ dotfiles/.config/hypr/modules/autostart.lua
@@ -5,4 +5,5 @@
 hl.on("hyprland.start", function () 
   hl.exec_cmd("nm-applet")
   hl.exec_cmd("waybar & hyprpaper & awww-daemon & swaync")
+  hl.exec_once("hyprctl setcursor Bibata-Modern-Classic 20")
 end)
--- dotfiles/.config/hypr/modules/environment.lua
+++ dotfiles/.config/hypr/modules/environment.lua
@@ -4,5 +4,6 @@
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("XCURSOR_SIZE", "20")
+hl.env("HYPRCURSOR_SIZE", "20")
